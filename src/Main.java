83e3e56d53767a55ca5487c3790c113ae7a2e39e
Karlz-Bandz <57764322+Karlz-Bandz@users.noreply.github.com>
2022-11-02 00:15:31 +0100

    MyCommit(1)

--- src/Main.java
+++ src/Main.java
@@ -10,43 +10,46 @@ public class Main {
         int number_of_tests;
         int tables;
         ArrayList<String> solutions = new ArrayList<>();
+        try {
 
-        System.out.println("Plese enter number of tests: ");
-        number_of_tests = scanner.nextInt();
+            System.out.println("Plese enter number of tests: ");
+            number_of_tests = scanner.nextInt();
 
-        for(int i = 0; i < number_of_tests; i++) {
+            for (int i = 0; i < number_of_tests; i++) {
 
-            ArrayList<SortClass> datas = new ArrayList<>();
+                ArrayList<SortClass> datas = new ArrayList<>();
 
-            System.out.println("Please enter number of tables: ");
-            tables = scanner.nextInt();
-            for (int j = 0; j < tables; j++) {
+                System.out.println("Please enter number of tables: ");
+                tables = scanner.nextInt();
+                for (int j = 0; j < tables; j++) {
 
-                String name;
-                int[] co_ordinates = new int[2];
+                    String name;
+                    int[] co_ordinates = new int[2];
 
-                System.out.println("Please enter the name of record: ");
-                name = scanner.next();
-                System.out.println("Please enter co-ordinates: ");
-                co_ordinates[0] = scanner.nextInt();
-                co_ordinates[1] = scanner.nextInt();
+                    System.out.println("Please enter the name of record: ");
+                    name = scanner.next();
+                    System.out.println("Please enter co-ordinates: ");
+                    co_ordinates[0] = scanner.nextInt();
+                    co_ordinates[1] = scanner.nextInt();
 
-                datas.add(new SortClass(name, co_ordinates));
+                    datas.add(new SortClass(name, co_ordinates));
+                }
 
+                SortAll sortAll = new SortAll(datas);
+                String solution = sortAll.repo.toString() + "\n";
+
+                solutions.add(solution);
             }
-
-            SortAll sortAll = new SortAll(datas);
-            String solution = sortAll.repo.toString() + "\n";
-
-            solutions.add(solution);
+        }catch (Exception e){
+            System.exit(420);
         }
 
         System.out.println("Sorted co_ordinates are below");
 
-
         for(int s = 0; s < solutions.size(); s++){
             System.out.println(solutions.get(s));
         }
+        System.exit(666);
     }
 }
 
--- src/SortClass.java
+++ src/SortClass.java
@@ -1,14 +1,18 @@
 import java.util.Arrays;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
 
 public class SortClass implements Comparable<SortClass>{
     private String name;
     private int[] co_ordinates = new int[2];
     private Double result;
 
+
     SortClass(String name, int[] co_ordinates){
         this.name = name;
         this.co_ordinates = co_ordinates;
-        this.result = Math.sqrt((co_ordinates[0]*co_ordinates[0])+(co_ordinates[1]*co_ordinates[1]));
+        this.result = getDistance.apply(co_ordinates[0], co_ordinates[1]);
     }
 
     public Double getResult(){
@@ -22,7 +26,12 @@ public class SortClass implements Comparable<SortClass>{
 
     @Override
     public int compareTo(SortClass o) {
-        int x = -(this.result.compareTo(o.result));
-        return x;
+        return getSort.apply(o.result);
     }
+
+    Function<Double, Integer> getSort = x -> -(this.result.compareTo(x));
+
+    BiFunction<Integer, Integer, Double> getDistance = (x, y) -> {
+        return  Math.sqrt((x*x) + (y*y));
+    };
 }
